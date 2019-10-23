83ca6d88983f2203aa341a57edd83dbd2f16ae23
Dominik Blank <DominikPBlank@t-online.de>
2019-10-23 14:03:45 +0200

    Performed stat significance analysis for test cases (24-28h computation time)

---
+++ CreateTestCases.m
@@ -0,0 +1,75 @@
+function cases = CreateTestCases(dims, nr_pictures, min_size)
+
+% Check for missing input and set, if necessary:
+if (~exist('dims', 'var'))
+    dims = [128, 256, 512];
+end
+if (~exist('nr_pictures', 'var'))
+    nr_pictures = 5;
+end
+if (~exist('min_size', 'var'))
+    min_size = 20;
+end
+
+% Create randomly generated ROI images:
+cases = createTestCases(dims, nr_pictures, min_size);
+
+% Plot generated ROI images:
+showTestCases(dims, nr_pictures, cases, 'f', 1);
+
+end
+
+function cases = createTestCases(dims, nr_pictures, min_size)
+
+% Initialize structure to store images and coordinates of the ROI:
+cases = struct;
+
+% Loop over dimensions to create:
+for i = 1 : size(dims, 2)
+    
+    % Generate name of the substructure of the dimension:
+    dim_name = strcat('pxl', num2str(dims(i)), 'x', num2str(dims(i)));
+    
+    for j = 1 : nr_pictures
+        
+        % Generate names of the image and coordinates:
+        fname = strcat('f', num2str(j));
+        tlcname = strcat('tlc', num2str(j));
+        brcname = strcat('brc', num2str(j));
+        
+        % Randomly generate ROI:
+        [ROI_Picture, tlc, brc] = ROI_Generator(dims(i), dims(i), min_size);
+        
+        % Store image and coordinates:
+        cases.(dim_name).(fname) = ROI_Picture;
+        cases.(dim_name).(tlcname) = tlc;
+        cases.(dim_name).(brcname) = brc;
+        
+    end
+end
+
+end
+
+function [] = showTestCases(dims, nr_pictures, cases, name, row)
+
+% Loop over dimensions:
+for i = 1 : size(dims, 2)
+    
+    % Start plot for dimension:
+    figure(i)
+    
+    % Generate name of the substructure of the dimension:
+    dim_name = strcat('pxl', num2str(dims(i)), 'x', num2str(dims(i)));
+    
+    for j = 1 : nr_pictures
+        
+        % Generate name of the image:
+        fname = strcat(name, num2str(j));
+        
+        % Plot the image:
+        subplot(row, nr_pictures, j), imshow(uint8(cases.(dim_name).(fname)));
+        
+    end
+end
+
+end
--- StatSignificanceTestCases.m
+++ StatSignificanceTestCases.m
@@ -1,65 +1,4 @@
-function cases = TestCases(type, dims, nr_pictures, min_size)
-
-% Check for missing input and set, if necessary:
-if (~exist('dims', 'var'))
-    dims = [128, 256, 512];
-end
-if (~exist('nr_pictures', 'var'))
-    nr_pictures = 5;
-end
-if (~exist('min_size', 'var'))
-    min_size = 20;
-end
-
-switch type
-    case 'create'
-        % Create randomly generated ROI images:
-        cases = createTestCases(dims, nr_pictures, min_size);
-        
-        % Plot ROI images:
-        showTestCases(dims, nr_pictures, cases, 'f', 1);
-    case 'load'
-        % Load input parameters and randomly generated ROI images:
-        load('ROI_TestCases.mat');
-        
-        % Perform statistical significance analysis for test cases:
-        cases = statSignificanceTestCases(dims, nr_pictures, cases, 0.05);
-end
-
-end
-
-function cases = createTestCases(dims, nr_pictures, min_size)
-
-% Initialize structure to store images and coordinates of the ROI:
-cases = struct;a
-
-% Loop over dimensions to create:
-for i = 1 : size(dims, 2)
-    
-    % Generate name of the substructure of the dimension:
-    dim_name = strcat('pxl', num2str(dims(i)), 'x', num2str(dims(i)));
-    
-    for j = 1 : nr_pictures
-        
-        % Generate names of the image and coordinates:
-        fname = strcat('f', num2str(j));
-        tlcname = strcat('tlc', num2str(j));
-        brcname = strcat('brc', num2str(j));
-        
-        % Randomly generate ROI:
-        [ROI_Picture, tlc, brc] = ROI_Generator(dims(i), dims(i), min_size);
-        
-        % Store image and coordinates:
-        cases.(dim_name).(fname) = ROI_Picture;
-        cases.(dim_name).(tlcname) = tlc;
-        cases.(dim_name).(brcname) = brc;
-        
-    end
-end
-
-end
-
-function cases = statSignificanceTestCases(dims, nr_pictures, cases, alpha)
+function cases = StatSignificanceTestCases(dims, nr_pictures, cases, alpha, max_nr_noise, max_sigma)
 
 % Load library of morphological operations:
 MO = MorphologicalOperations;
@@ -101,13 +40,13 @@ for i = 1 : size(dims, 2)
         err_Hoc_o = zeros(150, 2);
         
         % Repeat for multiple different randomly generated noises:
-        for nr_noise = 1 : 1
+        for nr_noise = 1 : max_nr_noise
             
             % Create standard normally distributed noise:
             noise = randn(dims(i), dims(i));
             
             % Loop over a range of standard deviations:
-            for sigma = 1 : 1
+            for sigma = 1 : max_sigma
                 
                 % Add noise to the image:
                 ROI_noisy = ROI_Picture + sigma * noise;
@@ -143,30 +82,6 @@ for i = 1 : size(dims, 2)
         cases.(dim_name).(strcat('err_FDR_o', num2str(j))) = err_FDR_o;
         cases.(dim_name).(strcat('err_Bon_o', num2str(j))) = err_Bon_o;
         cases.(dim_name).(strcat('err_Hoc_o', num2str(j))) = err_Hoc_o;
-        
-    end
-end
-
-end
-
-function [] = showTestCases(dims, nr_pictures, cases, name, row)
-
-% Loop over dimensions:
-for i = 1 : size(dims, 2)
-    
-    % Start plot for dimension:
-    figure(i)
-    
-    % Generate name of the substructure of the dimension:
-    dim_name = strcat('pxl', num2str(dims(i)), 'x', num2str(dims(i)));
-    
-    for j = 1 : nr_pictures
-        
-        % Generate name of the image:
-        fname = strcat(name, num2str(j));
-        
-        % Plot the image:
-        subplot(row, nr_pictures, j), imshow(uint8(cases.(dim_name).(fname)));
         
     end
 end
