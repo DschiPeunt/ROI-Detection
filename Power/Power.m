51eac921c7baf151efe38485f700491d0fad78d5
Dominik Blank <DominikPBlank@t-online.de>
2019-11-10 02:26:32 +0100

    Added graph to display bounds for power

---
+++ Power/Power.m
@@ -0,0 +1,21 @@
+% Initialize vectors for lower and upper bounds:
+lower_bound = zeros(1, size(sigma_range, 2));
+upper_bound = zeros(1, size(sigma_range, 2));
+
+% Calculate second argument for the Marcum Q-function:
+b = sqrt(- 2 * log(alpha));
+
+for sigma = sigma_range
+    % Calculate lower and upper bound:
+    lower_bound(1, sigma) = 1 - marcumq((2 * c_bg) / sigma, b);
+    upper_bound(1, sigma) = min(1, 2 * (1 - marcumq(c_bg / (sqrt(2) * sigma), b)));
+end
+
+% Plot lower and upper bounds:
+figure(1)
+hold on
+plot(sigma_range, lower_bound, 'Linewidth', 2.0)
+plot(sigma_range, upper_bound, 'Linewidth', 2.0)
+hold off
+legend('Lower bound', 'Upper bound', 'Location', 'southeast')
+title('Lower and upper bound for the power')
