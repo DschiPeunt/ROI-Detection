b63657445d534371f2babfd3033688a6423d5438
Dominik Blank <DominikPBlank@t-online.de>
2019-12-09 11:55:36 +0100

    Modified presentation

--- Notes/Matlab Resources/TheoreticalPower.m
+++ Notes/Matlab Resources/TheoreticalPower.m
@@ -1,3 +1,8 @@
+% Set values:
+alpha = 0.05;
+c_bg = 127.5;
+sigma_range = 1:150;
+
 % Initialize vectors for lower and upper bounds:
 lower_bound = zeros(1, size(sigma_range, 2));
 upper_bound = zeros(1, size(sigma_range, 2));
@@ -7,8 +12,8 @@ b = sqrt(- 2 * log(alpha));
 
 for sigma = sigma_range
     % Calculate lower and upper bound:
-    lower_bound(1, sigma) = 1 - marcumq((2 * c_bg) / sigma, b);
-    upper_bound(1, sigma) = min(1, 2 * (1 - marcumq(c_bg / (sqrt(2) * sigma), b)));
+    lower_bound(1, sigma) = marcumq((2 * c_bg) / sigma, b);
+    upper_bound(1, sigma) = 1 - min(1, 2 * (1 - marcumq(c_bg / (sqrt(2) * sigma), b)));
 end
 
 % Plot lower and upper bounds:
@@ -17,7 +22,6 @@ hold on
 plot(sigma_range, lower_bound, 'Linewidth', 2.0)
 plot(sigma_range, upper_bound, 'Linewidth', 2.0)
 hold off
-legend('Lower bound', 'Upper bound', 'Location', 'southeast')
-title('Lower and upper bound for the power')
+legend('Upper bound', 'Lower bound', 'Location', 'southeast')
 xlabel('sigma')
 ylabel('power')
